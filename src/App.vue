9115bb2f1cf7cd2c15017c9f0bf8ad1310c0fca7
divyansh7924 <guptadivyansh49@gmail.com>
2019-11-05 13:14:56 +0530

    Dashboard layout ready

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,40 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <cheader></cheader>
+    <dashboard></dashboard>
+    <router-view class="content-route"></router-view>
   </div>
 </template>
 
+<script>
+import dashboard from "./components/Dashboard.vue";
+import cheader from "./components/Header.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: { dashboard, cheader }
+};
+</script>
+
 <style>
-body {
-  margin: 0 !important;
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  background-color: white;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
 }
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -15,5 +43,14 @@ body {
   width: 100% !important;
   padding: 0 !important;
   background-color: white;
+  position: relative;
+}
+.content-route {
+  margin: 0 0 0 15%;
+  padding-top: 5% !important;
+  width: 85%;
+  height: 100vh;
+  overflow-x: auto;
+  overflow-y: scroll;
 }
 </style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+      <img src="../assets/icons/home.svg" class="icons-nav" />
+      Home
+    </router-link>
+    <router-link to="/profile">
+      <img src="../assets/icons/user.svg" class="icons-nav" />Users
+    </router-link>
+    <router-link to="/verifyofc">
+      <img src="../assets/icons/users.svg" class="icons-nav" />Sales Person
+    </router-link>
+    <router-link to="/verifyfra">
+      <img src="../assets/icons/franchisee.svg" class="icons-nav" />Franchise
+    </router-link>
+    <router-link to="/batteryreg">
+      <img src="../assets/icons/other.svg" class="icons-nav" />Others
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+#nav {
+  height: 100vh;
+  width: 15%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding-top: 5%;
+  border-right: #dddddd solid 1px;
+  z-index: 1;
+}
+#nav a {
+  font-weight: bold;
+  color: #b1b1b1;
+  text-decoration: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px 30px;
+  width: 100%;
+}
+#nav a.router-link-exact-active:after {
+  content: "";
+  display: block;
+  position: absolute;
+  left: 0;
+  width: 50%;
+  padding-top: 25px;
+  border-bottom: 1.5px solid #dddddd;
+}
+.icons-nav {
+  margin-right: 10px;
+}
+</style>
